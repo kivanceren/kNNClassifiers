2729e255763b4f82a02f3350f66fe88a0f9c534c
kivanceren <kivanceren@yahoo.com>
2017-03-24 15:22:58 +0300

    Fall Detection için veri seti denendi.

--- FallDetection/src/com/main/kivanc/FallDetectionKNN.java
+++ FallDetection/src/com/main/kivanc/FallDetectionKNN.java
@@ -1,11 +1,57 @@
 package com.main.kivanc;
 
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import com.classifiers.kivanc.KnnClassifier;
+import com.datastructre.kivanc.Data;
+import com.exceptions.kivanc.IncorrectParameterException;
+
 public class FallDetectionKNN {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ReadFromFile readFromFile = new ReadFromFile();
 		
-		System.out.println("Hello KNN");
+		List<Data> dataSet = readFromFile.getDataFromFile(new File("dataSet.txt"), 4);
+		
+	    
+	  
+	    Collections.shuffle(dataSet);
+	    
+	    List<Data> divideDataSet, divideTestSet;
+	    
+	    int percent66 = dataSet.size()*66/100;
+	    
+	    divideDataSet = dataSet.subList(0, percent66);
+	    divideTestSet = dataSet.subList(percent66, dataSet.size());
+	    
+	    double attr[] = {0.186, 4.12, 8.683, 9.61267314538469};
+	    Data data = new Data(attr, 0);
+	    
+	    /*double attr[] = {2.45,-0.703, 13.91, 14.142639251568287};
+	    Data data = new Data(attr, 0);*/
+	    
+	    KnnClassifier knnClassifier = new KnnClassifier(0, 5, 2, divideDataSet, divideTestSet);
+	    
+	    try {
+	    	
+			   int classLabel = knnClassifier.kNNClassifierMethod(data);
+			   System.out.println("Class is:" + classLabel);
+			   knnClassifier.createConfusionMatrix();
+			   knnClassifier.printConfusionMatrix();		   
+			   System.out.println(knnClassifier.accuaryOfClassifiers() );
+			   
+		} catch (IllegalStateException | IncorrectParameterException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
 	}
 
 }

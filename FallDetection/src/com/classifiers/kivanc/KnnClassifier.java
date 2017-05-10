a7619b9260505337141e2a9c88541dd41dee8bc2
kivanceren <kivanceren@yahoo.com>
2017-05-10 17:15:05 +0300

    KNN sınıflandırıcısına her sınıfa ait sınıflandırma başarımlarını elde etme özelliği yapıldı.

--- FallDetection/src/com/classifiers/kivanc/KnnClassifier.java
+++ FallDetection/src/com/classifiers/kivanc/KnnClassifier.java
@@ -26,6 +26,8 @@ public class KnnClassifier {
 	private int [][]confusionMatrix;
 	private static Map<Integer, String> classMap;
 	private String lastClassfiedInstaceClass;
+	private double[] accuaryOfClass;
+	private double accuaryOfClassifier = -1;
 	
 	public void setClassLabels(String[] classNames){
 		classMap = new HashMap<Integer,String>();  
@@ -40,7 +42,7 @@ public class KnnClassifier {
 		this.metric_Type = 1;
 		this.paramNeighbor = 1;
 		this.classNumber = cN;
-		this.confusionMatrix = new int[classNumber][classNumber];
+		setParamater();
 		this.testSet = null;
 	}
 	
@@ -50,7 +52,7 @@ public class KnnClassifier {
 		this.metric_Type = 1;
 		this.paramNeighbor = 1;
 		this.classNumber = cN;
-		this.confusionMatrix = new int[classNumber][classNumber];
+		setParamater();
 		this.testSet = null;
 		this.dataSet = dataSet;
 		
@@ -62,7 +64,7 @@ public class KnnClassifier {
 		this.metric_Type = 1;
 		this.paramNeighbor = 1;
 		this.classNumber = cN;
-		this.confusionMatrix = new int[classNumber][classNumber];
+		setParamater();
 		this.testSet = null;
 		this.dataSet = dataSet;
 		this.testSet = testSet;
@@ -80,7 +82,7 @@ public class KnnClassifier {
 		if(classNumber<=0) throw new InvalidParameterException();
 		else this.classNumber = classNumber;
 		
-		this.confusionMatrix = new int[classNumber][classNumber];
+		setParamater();
 		
 		this.dataSet = dataSet;
 	}
@@ -97,37 +99,13 @@ public class KnnClassifier {
 		if(classNumber<=0) throw new InvalidParameterException();
 		else this.classNumber = classNumber;
 		
-		this.confusionMatrix = new int[classNumber][classNumber];
+		 setParamater();
 		
 		this.dataSet = dataSet;
 		this.testSet = testSet;
 	}
 	
-	public void setClassifierParameters(int metric_Type, int paramNeighbor, int classNumber) throws InvalidParameterException
-	{
-		
-		if(metric_Type>=0 && metric_Type<=1)
-								this.metric_Type = metric_Type;
-		else metric_Type=0;
-		
-		if(paramNeighbor<1) paramNeighbor=1;
-		else this.paramNeighbor = paramNeighbor;
-		
-		if(classNumber<=0) throw new InvalidParameterException();
-		else this.classNumber = classNumber;
-	}
-	
-	public void addDataSets(List<Data> dataSet)
-	{
-		this.dataSet = dataSet;
-	}
-	
-	public void addDataSets(List<Data> dataSet, List<Data> testSet)
-	{
-		this.dataSet = dataSet;
-		this.testSet = testSet;
-	}
-	
+
 	public int kNNClassifierMethod(Data instance) throws IllegalStateException, IncorrectParameterException
 	{
 		//data setin alınması ve ilgili kontrollerin yapılması ile distance tiplerinin beliritlmesi
@@ -159,7 +137,6 @@ public class KnnClassifier {
 		return classOfInstace;
 	}
 	
-
 	
 	public String getClassName(){return lastClassfiedInstaceClass;}
 
@@ -200,35 +177,87 @@ public class KnnClassifier {
 		if(this.testSet.isEmpty()){
 				System.out.print("testSet boş ConfusionMatrix oluşturulamıyor...");
 			    return;
-			}	
+			}
+		if(confusionMatrix != null) return;
+		confusionMatrix = new int[classNumber][classNumber];
 		for(int i=0; i<testSet.size(); i++){
-			
 			Data testData = testSet.get(i);
 			int testDataClass = testData.getClassLabel();
 			int kNN_Class = this.kNNClassifierMethod(testData);
 			confusionMatrix[testDataClass][kNN_Class]++;	
-			
-		}
+			if(testDataClass == kNN_Class) accuaryOfClassifier++;
+		}	
+		accuaryOfClassifier = (accuaryOfClassifier*100) / testSet.size();
 	}
 	
-	public String accuaryOfClassifiers()
-	{
-		int numberOfTestSet = this.testSet.size();
+	public double[] accuaryOfEachClasses() throws IncorrectParameterException{
 		
-		int correctClassifying = 0;
-		if(numberOfTestSet == 0 ) return "testSet sayısı Yetersiz!!!";//assert ile yazılabilir.
+		if(classNumber < 0) throw new IncorrectParameterException("Sınıf sayısı geçersiz. 0'dan büyük olmalı.");
 		
-		for(int i=0 ; i <classNumber; i++)
-					correctClassifying += confusionMatrix[i][i];
+		if(accuaryOfClass != null) return accuaryOfClass; //zaten varsa yeniden oluşturmadan dön.
+		
+		accuaryOfClassifier = 0;
+		int counter = 0;
+		
+		if (confusionMatrix != null){
+			accuaryOfClass = new double[classNumber];
+			double matchCount, eachCount;
+			for(int i=0; i<confusionMatrix.length; i++ ){
+				matchCount = 0;
+				eachCount = 0;
+				for(int j=0; j<confusionMatrix[i].length; j++){
+					eachCount += confusionMatrix[i][j];
+					counter++;
+					if(i==j) {
+								accuaryOfClassifier++;
+								matchCount+= confusionMatrix[i][j];
+							}
+				}
+				accuaryOfClass[i] = (matchCount*100/eachCount);
+			}
+			accuaryOfClassifier = (accuaryOfClassifier*100) / counter;
+			return accuaryOfClass;
+		}
 		
 		
-		double accuary = (correctClassifying*100)/numberOfTestSet;  
+		int[] correct = new int[classNumber];
+		int[] records = new int[classNumber];
 		
-		return "Accuary is %" + accuary + "." ;
+		accuaryOfClass = null;
+		accuaryOfClass = new double [classNumber];
+		
+		for(int i=0; i<testSet.size(); i++){
+			Data testData = testSet.get(i);
+			int testDataClass = testData.getClassLabel();
+			int kNN_Class = this.kNNClassifierMethod(testData);
+			if(testDataClass == kNN_Class){ 
+											 accuaryOfClassifier++;
+											 correct[testDataClass]++;
+											}
+			counter++;
+			records[testDataClass]++;
+		}
+		
+		accuaryOfClassifier = (accuaryOfClassifier*100) / counter;
+		
+		for(int i=0; i<classNumber; i++)
+			accuaryOfClass[i] = ((double)correct[i]*100)/ (double)records[i];
+		
+		correct = null;
+		records = null;
+		
+		
+		return accuaryOfClass;
 	}
+	
+
 	
 	public void printConfusionMatrix()
 	{
+		if ( confusionMatrix == null ){
+			System.out.println("Confusion Matrix oluşturulmamış.");
+			return ;
+		}
 		for(int i=0; i<classNumber; i++ )
 		{
 			for(int j=0; j<classNumber; j++){
@@ -245,14 +274,17 @@ public class KnnClassifier {
 
 	public void setMetric_Type(int metric_Type) {
 		this.metric_Type = metric_Type;
+	    setParamater();
 	}
 
 	public int getParamNeighbor() {
 		return paramNeighbor;
+		
 	}
 
 	public void setParamNeighbor(int paramNeighbor) {
 		this.paramNeighbor = paramNeighbor;
+		 setParamater();
 	}
 
 	public List<Data> getDataSet() {
@@ -261,7 +293,8 @@ public class KnnClassifier {
 
 	public void setDataSet(List<Data> dataSet) {
 		this.dataSet = dataSet;
-	}
+		 setParamater();
+	} 
 
 	public List<Data> getTestSet() {
 		return testSet;
@@ -269,6 +302,24 @@ public class KnnClassifier {
 
 	public void setTestSet(List<Data> testSet) {
 		this.testSet = testSet;
+		setParamater();
+		
 	}	
+	
+	public double[] getAccuaryOfEachClasses()
+	{
+		return accuaryOfClass;
+	}
+	
+	public double getAccuaryOfClassifier()
+	{
+		return accuaryOfClassifier;
+	}
+	
+	public void setParamater(){ //ilgili parametreler değişirse knndeki sıfırlanması gerekenleri sıfırlar.
+		this.confusionMatrix = null;
+		this.accuaryOfClass = null;
+		this.accuaryOfClassifier = -1;
+	}
 
 }
